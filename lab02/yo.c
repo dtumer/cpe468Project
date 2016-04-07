559f3001106d533e01d647d8b97eb0c0b81f8c34
Deniz Tumer <e.tumer.deniz@gmail.com>
2016-04-06 17:41:34 -0700

    added a file

---
+++ lab02/yo.c
@@ -0,0 +1,9 @@
+//
+//  yo.c
+//  lab02
+//
+//  Created by Deniz Tumer on 4/6/16.
+//
+//
+
+#include "yo.h"
---
+++ lab02/yo.h
@@ -0,0 +1,14 @@
+//
+//  yo.h
+//  lab02
+//
+//  Created by Deniz Tumer on 4/6/16.
+//
+//
+
+#ifndef yo_h
+#define yo_h
+
+#include <stdio.h>
+
+#endif /* yo_h */
