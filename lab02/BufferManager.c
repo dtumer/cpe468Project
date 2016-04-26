d0e65c2ddcb4d42e26e4fe1559b8b026a6778648
David Ellison <davidellison341@gmail.com>
2016-04-25 19:02:48 -0700

    compile error

--- lab02/BufferManager.c
+++ lab02/BufferManager.c
@@ -555,7 +555,7 @@ int removeCachePage(Buffer *buf, DiskAddress diskPage) {
    Block *cachePage;
    int result;
     //check cache
-   result = getIndex(cacheMap, diskPage)
+   result = getIndex(cacheMap, diskPage);
    if (result != -1) {
      /* page is in volatile storage */
       cachePage = buf->cache[result];
@@ -573,13 +573,17 @@ int removeCachePage(Buffer *buf, DiskAddress diskPage) {
          cachePage = buf->pages[result];
          free(cachePage);
          removeIndex(bufMap, diskPage);
+         /* move the last entry in the buffer to the vacated slot*/
          buf->pages[result] = buf->pages[buf->numBufferOccupied - 1];
          removeIndex(bufMap, buf->pages[buf->numBufferOccupied - 1]->diskAddress);
          putIndex(bufMap, buf->pages[result]->diskAddress);
          buf->pages[buf->numBufferOccupied - 1] = NULL;
          buf->numBufferOccupied--;
          
-         /* move the last entry in the buffer to the vacated slot*/
+         
+      } else {
+         /* the page was evicted to disk from the buffer, or it never existed*/
+         /* what do? */
       }
    }
     
