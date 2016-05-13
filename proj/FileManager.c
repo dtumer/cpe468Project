aae63d7da4e694be217de4d966edef1a882948af
Deniz Tumer <dtumer@csclnx11.ad.calpoly.edu>
2016-05-12 20:21:12 -0700

    added the function getFirstFreeRecord which gets the first free record from the header bitmap that keeps track of free records on a disk page

--- proj/FileManager.c
+++ proj/FileManager.c
@@ -1,8 +1,33 @@
 #include "FileManager.h"
 
 //returns the index of the first free record in the bitmap
-int getFirstFreeRecord(char *bitmap, int numRecords) {
-	return 0;
+int getFirstFreeRecord(uint8_t *bitmap, int numRecords) {
+	int result, counter = 0;
+	uint16_t mask = 128;
+	uint8_t *temp;
+	
+	if (numRecords <= 0) {
+		return -1;
+	}
+	
+	if (*bitmap == 0xff) {
+		result = getFirstFreeRecord(bitmap + 1, numRecords - 8);
+		
+		if (result == -1) {
+			return -1;
+		}
+		else {
+			return result + 8;
+		}
+	}
+	
+	temp = bitmap;
+	while (mask & *temp) {
+		mask >>= 1;
+		counter++;
+	}
+	
+	return counter;
 }
 
 //calculates the size of the bitmap given a page
@@ -20,6 +45,8 @@ int calcBitmapSize(int recordSize, int pageSize, int headerSize, int curBitmapSi
 	if (newHeaderSize % 8 > 0) {
 		newBitmapSize += 8 - newHeaderSize % 8;
 	}
+	
+	printf("New Bitmap Size: %d\n", newBitmapSize);
 	
 	if (curBitmapSize == newBitmapSize) {
 		return curBitmapSize;
