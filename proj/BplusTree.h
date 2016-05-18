570bb935190bf3c004e6cd7d98520be9ccdbb69f
Jonathon Elfar <jelfar@csclnx11.ad.calpoly.edu>
2016-05-17 19:20:05 -0700

    added function headers for B+trees

--- proj/BplusTree.h
+++ proj/BplusTree.h
@@ -11,4 +11,10 @@
 
 #include <stdio.h>
 
+int createBTree(char *table, RecordStructure key, int volatileFlag);
+int dropBTree(char *btreeName);
+int bTreeInsert(char *btree, char *record);
+int bTreeDelete(char *btree, char *key);
+int bTreeFind(char *btree, char *key, DiskAddress * page);
+
 #endif /* BplusTree_h */
