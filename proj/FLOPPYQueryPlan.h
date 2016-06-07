cf4fc3e40ad78acab3385180a31634faa2ae20fb
David Ellison <davidellison341@gmail.com>
2016-06-07 01:33:45 -0700

    struct for tree node, gonna change that

--- proj/FLOPPYQueryPlan.h
+++ proj/FLOPPYQueryPlan.h
@@ -3,6 +3,21 @@
 #ifndef FLOPPYQueryPlan_h
 #define FLOPPYQueryPlan_h
 
+//incomplete list!
+//TODO fill it out and figure out data representations for arguments;
+//ie columns for grouping, attribute list for projection
+typedef {
+   SELECTION,
+   PROJECTION,
+   DEDUPLICATE,
+   RENAME,
+   CROSS,
+   SORT,
+   GROUP,
+   TABLE
+   } RelationalOperation;
+
+
 class FLOPPYQueryPlan {
 public:
 	/* Constructors */
@@ -13,15 +28,20 @@ public:
     /* Descructor */
     virtual ~FLOPPYQueryPlan();
     
-    /* methods to create a query plan*/
-    createSelectQueryPlan(FLOPPYSelectStatement *stmt);
-    create
+    
+    //inner class for tree nodes
+    struct QueryNode {
+       //if TABLE, there are no operands, just a name; both the child pointers should
+       //be NULL. if not TABLE, tablename should be NULL.
+       RelationalOperation op;
+       char *tablename = NULL;
+       
+       void *operationData = NULL;       
+       QueryNode *firstChild = NULL;
+       QueryNode *secondChild = NULL;
+    }
     
     /* Instance Variables */
-    char *name;
-    ColumnType type;
-    int size;
-    int offset;
     
 
 	
