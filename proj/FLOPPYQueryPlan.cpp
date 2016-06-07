570334f13e49634eae8b1a54886f34a05fc2e2bd
David Ellison <davidellison341@gmail.com>
2016-06-07 01:33:45 -0700

    struct for tree node, gonna change that

--- proj/FLOPPYQueryPlan.cpp
+++ proj/FLOPPYQueryPlan.cpp
@@ -1,2 +1,14 @@
 //FLOPPYQueryPlan.cpp
 
+/**
+* Create a FLOPPY query plan based on the statement 
+*/
+FLOPPYQueryPlan(FLOPPYStatement *statement) {
+   //move the big switch statement from FLOPPY_DBMDS here.
+   
+}
+
+//free all nodes in the tree, and any allocated instance variables
+~FLOPPYQueryPlan() {
+
+}
