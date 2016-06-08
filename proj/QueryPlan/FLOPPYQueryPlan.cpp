f60ff113fed86f7bedb51266ceac9f5dc276148c
David Ellison <davidellison341@gmail.com>
2016-06-08 11:58:24 -0700

    fixed small things but still broken

--- proj/QueryPlan/FLOPPYQueryPlan.cpp
+++ proj/QueryPlan/FLOPPYQueryPlan.cpp
@@ -3,9 +3,44 @@
 /**
 * Create a FLOPPY query plan based on the statement 
 */
-FLOPPYQueryPlan(FLOPPYStatement *statement) {
-   //move the big switch statement from FLOPPY_DBMDS here.
+FLOPPYQueryPlan::FLOPPYQueryPlan(FLOPPYStatement *statement) {
    
+   switch (statement->type()) {
+            case StatementType::ErrorStatement:
+                break;
+            case StatementType::CreateTableStatement:
+                result = createTable((FLOPPYCreateTableStatement*) parsedCommand->statement);
+                break;
+            case StatementType::DropTableStatement:
+                printf("DROP TABLE\n");
+                printDropTableStatement((FLOPPYDropTableStatement*) parsedCommand->statement);
+                break;
+            case StatementType::CreateIndexStatement:
+                printf("CREATE INDEX\n");
+                //printCreateIndexStatement((FLOPPYCreateIndexStatement*) parsedCommand->statement);
+                break;
+            case StatementType::DropIndexStatement:
+                printf("DROP INDEX\n");
+                printDropIndexStatement((FLOPPYDropIndexStatement*) parsedCommand->statement);
+                break;
+            case StatementType::InsertStatement:
+                printf("INSERT\n");
+                printInsertStatement((FLOPPYInsertStatement*) parsedCommand->statement);
+                break;
+            case StatementType::DeleteStatement:
+                printf("DELETE\n");
+                printDeleteStatement((FLOPPYDeleteStatement*) parsedCommand->statement);
+                break;
+            case StatementType::UpdateStatement:
+                printf("UPDATE\n");
+                printUpdateStatement((FLOPPYUpdateStatement*) parsedCommand->statement);
+                break;
+            case StatementType::SelectStatement:
+                printf("SELECT\n");
+                root = new SelectionNode();
+                printSelectStatement((FLOPPYSelectStatement*) parsedCommand->statement);
+                break;
+        }
 }
 
 //free all nodes in the tree, and any allocated instance variables
